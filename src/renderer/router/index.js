0d241fce570b0378c9f13e0ad6fc3c482f4516d6
jiangliyue <772748611@qq.com>
2021-01-21 14:09:12 +0800

    index

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -7,8 +7,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'landing-page',
-      component: require('@/components/landingPage').default
+      name: 'index',
+      component: () => import('@/views/index')
     },
     {
       path: '*',
