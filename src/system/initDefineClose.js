2e04ff7ccac6655cb157d7504439b9256c35ae51
jiangliyue <772748611@qq.com>
2021-01-26 14:04:55 +0800

    close

--- src/system/initDefineClose.js
+++ src/system/initDefineClose.js
@@ -1,11 +1,9 @@
 function initDefineClose(app, mainWindow) {
     mainWindow.on('close', (e) => {
         e.preventDefault();		// 阻止默认行为
-        mainWindow.hide()
-        if(mainWindow.isMinimized()){
+        if (mainWindow.isMinimized()){
             mainWindow = null;
-        }else{
-            e.preventDefault();
+        } else {
             mainWindow.minimize();
         }
     })
