63987a9a0764e9aaeeca651e5216ce7b7c166bed
jiangliyue <772748611@qq.com>
2021-02-19 23:03:43 +0800

    config

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,10 @@
 const config = {
-    updateUrl: 'http://116.85.5.71:8080/', // 检测更新地址
     isAutoOpen: true, // 开机自启
-    isAutoUpdate: true, // 自动更新
     isOnlyOpen: true, // 窗口唯一
     isUseTray: true, // 使用托盘
     isDefineClose: true, // 自定义关闭按钮(最小化禁用关闭事件)
+    isAutoUpdate: false, // 自动更新
+    updateUrl: 'http://116.85.5.71:8080/', // 检测更新地址
 }
 
 export default config
