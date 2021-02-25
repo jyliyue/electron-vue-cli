fc7d4d5fec6d94acfdafeb7b5df0a2572cb1dcd5
jiangliyue <772748611@qq.com>
2021-02-25 23:56:02 +0800

    update-url

--- src/config/index.js
+++ src/config/index.js
@@ -1,14 +1,14 @@
-const { name, version } = require('../../package.json')
+const { build, version } = require('../../package.json')
 
 const config = {
     version: version,  // 版本号
-    appName: name, // 鼠标悬停显示
+    appName: build.productName, // 鼠标悬停显示
     isAutoOpen: true, // 开机自启
     isOnlyOpen: true, // 窗口唯一
     isUseTray: true, // 使用托盘
     isDefineClose: true, // 自定义关闭按钮(最小化禁用关闭事件)
     isAutoUpdate: true, // 自动更新
-    updateUrl: 'http://116.85.5.71:8080/', // 检测更新地址
+    updateUrl: build.publish[0].url, // 检测更新地址
 }
 
 export default config
