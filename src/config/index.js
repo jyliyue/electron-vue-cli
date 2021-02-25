d1191c3bfba2cc7676120d9cb8236ed37ff970f3
jiangliyue <772748611@qq.com>
2021-02-25 23:29:21 +0800

    update

--- src/config/index.js
+++ src/config/index.js
@@ -4,7 +4,7 @@ const config = {
     isOnlyOpen: true, // 窗口唯一
     isUseTray: true, // 使用托盘
     isDefineClose: true, // 自定义关闭按钮(最小化禁用关闭事件)
-    isAutoUpdate: false, // 自动更新
+    isAutoUpdate: true, // 自动更新
     updateUrl: 'http://116.85.5.71:8080/', // 检测更新地址
 }
 
